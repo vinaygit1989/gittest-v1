a8df4ceb85a1b8beede6b3cd533383367a9cc8a6
tsm <you@example.com>
2022-08-11 19:38:36 +0200

    first commit

---
+++ src/test/java/com/pav/gitest/GitestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pav.gitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
